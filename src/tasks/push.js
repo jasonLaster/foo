b240a120622c276e9b956a5fe0363ec6c746fe44
Jason Laster <jason.laster.11@gmail.com>
2017-10-27 09:35:24 -0400

    add push code

---
+++ src/tasks/push.js
@@ -0,0 +1,27 @@
+function getRemote() {
+  const { stdout } = gitCmd(`branch -vv`);
+
+  // check for a remote/branch pair
+  const matches = stdout.match(/\*.*\[(\S+)\].*/);
+
+  if (!matches) {
+    const [, branch] = gitCmd(`branch`).stdout.match(/\* (\S+).*/);
+    return { remote: null, branch };
+  }
+
+  const [remote, branch] = remoteBranch.split("/");
+  return { remote, branch };
+}
+
+function push() {
+  const { remote, branch } = getRemote();
+
+  if (remote) {
+    gitCmd(`push --no-verify -f ${remote} ${branch} `);
+    return;
+  }
+
+  gitCmd(`push --no-verify -f me ${branch} `);
+}
+
+module.exports = { getRemote, push };
