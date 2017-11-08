1606b3ed2a7630e7221248510333985cec49d3b5
Jason Laster <jason.laster.11@gmail.com>
2017-11-08 12:04:46 -0500

    support : in branch pair

--- src/tasks/push.js
+++ src/tasks/push.js
@@ -1,11 +1,16 @@
 const { action } = require("hankey");
 const { gitCmd } = require("../utils");
 
+/*
+ *  handles both cases w/ the : and and w/o
+ * license               b12187b [khal0988/license] added license header to all files in src/workers
+ * flow_0.57.3           e306387 [arthur801031/flow_0.57.3: ahead 11, behind 4] Upgrade flow
+*/
 function getRemote() {
   const { stdout } = gitCmd(`branch -vv`);
 
   // check for a remote/branch pair
-  const matches = stdout.match(/\*.*\[(\S+)\].*/);
+  const matches = stdout.match(/\*.*\[(\S+)(\]|:).*/);
 
   if (!matches) {
     const [, branch] = gitCmd(`branch`).stdout.match(/\* (\S+).*/);
